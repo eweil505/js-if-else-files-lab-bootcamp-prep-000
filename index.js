f0a8e68bb52178c89f304b6445549667237ede3a
Erika Weil <erikaweil@outlook.com>
2017-08-29 14:36:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var height = "74"
 //   console.log("The name is correct");
 // } else {
 //   console.log("Expected the name to be Susan")
-}
+// }
 // Don't worry about this
 module.exports = { name, height
 }
